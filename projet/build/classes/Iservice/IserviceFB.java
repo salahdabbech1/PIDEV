3cb52878716a6aa90e0fd1bc095f32ced722ee26
oueslatikarim <47775685+oueslatikarim@users.noreply.github.com>
2021-03-21 01:00:56 +0100

    Add files via upload

---
+++ projet/build/classes/Iservice/IserviceFB.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Iservice;
+
+import projet.entite.user;
+
+/**
+ *
+ * @author KARIMOOOO
+ */
+public interface IserviceFB {
+    public boolean InscriptionFB(user u);
+}
